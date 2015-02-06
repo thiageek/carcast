14608866f1fb503d2279a4ea81b0877d16e822c1
Thiago Pinheiro <thiagopinheirosilva@gmail.com>
2015-02-06 03:39:01 -0200

    Atualizando dados de teste em tab-audio

--- www/js/services.js
+++ www/js/services.js
@@ -6,29 +6,18 @@
   // Some fake testing data
   var audios = [{
     id: 0,
-    name: 'Ben Sparrow',
-    lastText: 'You on your way?',
-    face: 'https://pbs.twimg.com/profile_images/514549811765211136/9SgAuHeY.png'
+    title: 'Nerdcast Empreendedor 01',
+    subtitle: 'O valor de uma ideia',
+    author: 'Jovem Nerd',
+    image: 'img/debug/jn.jpg',
+    category: 'podcast'
   }, {
     id: 1,
-    name: 'Max Lynx',
-    lastText: 'Hey, it\'s me',
-    face: 'https://avatars3.githubusercontent.com/u/11214?v=3&s=460'
-  }, {
-    id: 2,
-    name: 'Andrew Jostlin',
-    lastText: 'Did you get the ice cream?',
-    face: 'https://pbs.twimg.com/profile_images/491274378181488640/Tti0fFVJ.jpeg'
-  }, {
-    id: 3,
-    name: 'Adam Bradleyson',
-    lastText: 'I should buy a boat',
-    face: 'https://pbs.twimg.com/profile_images/479090794058379264/84TKj_qa.jpeg'
-  }, {
-    id: 4,
-    name: 'Perry Governor',
-    lastText: 'Look at my mukluks!',
-    face: 'https://pbs.twimg.com/profile_images/491995398135767040/ie2Z_V6e.jpeg'
+    title: 'Geração de Valor',
+    subtitle: 'Compartilhando inspiração',
+    author: 'Flávio Augusto',
+    image: 'img/debug/gv.jpg',
+    category: 'audiobook'
   }];
 
   return {
